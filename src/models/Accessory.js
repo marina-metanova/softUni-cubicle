e4957102862fd855a7c602cfb355874b603e5b79
Marina <marina@eurocoders.com>
2022-06-12 15:37:36 +0300

    Create Accessory Model

---
+++ src/models/Accessory.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+        validate: {
+            // validator: /^htttp?/g,
+            validator: function() {
+                return this.imageURL.startsWith('http');
+            },
+            message: 'Image urlw should start with http/s'
+        }
+    },
+    description: {
+        type: String,
+        maxLength: 120,
+        required: true,
+    }
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
